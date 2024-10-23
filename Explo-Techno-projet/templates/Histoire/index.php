d01d7262b9f815be08f2b0d0f016faa6221cecc7
Alan Vestis <alan.vestis56@gmail.com>
2024-10-23 10:09:48 -0400

    Partie Alan
    Histoire

---
+++ Explo-Techno-projet/templates/Histoire/index.php
@@ -0,0 +1,25 @@
+<h1>Histoire</h1>
+<a href="templates/Accueil"><button>Acceuil</button></a>
+<br>
+<h4>Semaine 1 :</h4>
+    <h5>Cours 1 :<h5>
+        <p>Introduction au Python</p>
+    <h5>Cours 2 :</h5>
+        <p>Introduction IOT et Raspberry</p>
+<br>
+<h4>Semaine 2 :</h4>
+	<p>Découverte de git hub desktop</p>
+<br>
+<h4>Semaine 3 :
+	<p>Outils collaboratives: Planner, One Drive, SharePoint</p>
+<br>
+<h4>Semaine 4 :
+	<p>Cryptographie : Sécurisation des échanges entre deux entités</p>
+<br>
+<h4>Semaine 5 :
+	<p>Sécurisation web</p>
+<br>
+<h4>Semaine 6 :
+	<p>Découverte d’un Framework qu’on a choisi préalablement</p>
+
+<h3>Le moment que l’on a le plus apprécié c’est celui avec la pratique du Raspberry (le code, le câblage et le serveur)</h3>
