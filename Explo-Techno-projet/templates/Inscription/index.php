49a7168f9b44f5bc7d36dc33026651ff8b364d1d
Alan Vestis <alan.vestis56@gmail.com>
2024-11-06 10:56:42 -0500

    HTML

---
+++ Explo-Techno-projet/templates/Inscription/index.php
@@ -0,0 +1,14 @@
+<div class="bloc-connexion-inscription">
+    <form action="">
+        <h2>Inscription :</h2>
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" name="username" id="username">
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" id="password">
+        <label for="role">Rôle:</label>
+        <select name="role" id="role">
+          <option value="visiteur">Visiteur</option>
+          <option value="utilisateur">Utilisateur</option>
+        </select>
+    </form>
+</div>
