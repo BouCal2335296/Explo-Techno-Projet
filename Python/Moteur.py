b8608ddd8e5b879d8ad5cc2b74568843730a34c2
Calvin Bourdet <calvin.bourdet64@gmail.com>
2024-11-06 09:29:06 -0500

    modif moteur.py

--- Python/Moteur.py
+++ Python/Moteur.py
@@ -6,34 +6,51 @@ GPIO.setmode(GPIO.BCM)  # Utilise la numérotation des broches GPIO (BCM)
 GPIO.setwarnings(False)
  
 # Broche à laquelle le FS90R est connecté
-servo_pin = 18  # Assurez-vous que la broche est correcte pour votre configuration
+servo_pin = 16 # Assurez-vous que la broche est correcte pour votre configuration
 GPIO.setup(servo_pin, GPIO.OUT)
  
 # Configuration du PWM sur la broche du servo
-pwm = GPIO.PWM(servo_pin, 50)  # Fréquence de 50Hz pour le servo moteur
+pwm = GPIO.PWM(servo_pin, 20)  # Fréquence de 50Hz pour le servo moteur
 pwm.start(0)  # Démarre avec un rapport cyclique de 0 (ne bouge pas le moteur)
  
 # Fonctions pour contrôler le moteur
-def tourner_droite():
+def tourner_A():
+    pwm.ChangeDutyCycle(7.5)  # Ajustez la valeur pour la rotation à droite
+    time.sleep(1)
+
+def tourner_B():
+
+    pwm.ChangeDutyCycle(7.5)  # Ajustez la valeur pour la rotation à droite
+    time.sleep(1)
+
+def tourner_C():
+    pwm.ChangeDutyCycle(7.5)  # Ajustez la valeur pour la rotation à droite
+    time.sleep(1)
+
+def tourner_D():
+    pwm.ChangeDutyCycle(7.5)  # Ajustez la valeur pour la rotation à droite
+    time.sleep(1)
+
+def tourner_RAZ():
     pwm.ChangeDutyCycle(7.5)  # Ajustez la valeur pour la rotation à droite
     time.sleep(1)
  
-def tourner_gauche():
-    pwm.ChangeDutyCycle(2.5)  # Ajustez la valeur pour la rotation à gauche
-    time.sleep(1)
- 
-def arret():
-    pwm.ChangeDutyCycle(0)  # Arrête le moteur
-    time.sleep(1)
- 
-# Exemple d'utilisation
+
 try:
     while True:
-        tourner_droite()
+        tourner_A()
         time.sleep(1)
-        tourner_gauche()
+
+        tourner_B()
         time.sleep(1)
-        arret()
+
+        tourner_C()
+        time.sleep(1)
+
+        tourner_D()
+        time.sleep(1)
+
+        tourner_RAZ()
         time.sleep(1)
  
 except KeyboardInterrupt:
