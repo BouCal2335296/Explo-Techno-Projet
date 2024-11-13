225014909c5f922d7e7d8371f5a5a8da27eaf355
Calvin Bourdet <143567950+BouCal2335296@users.noreply.github.com>
2024-11-13 11:34:46 -0500

    Push

--- Explo-Techno-projet/templates/Connexion/index.php
+++ Explo-Techno-projet/templates/Connexion/index.php
@@ -1,6 +1,6 @@
 <div class="div-arrondi">
     <h2>Connexion</h2>
-    <form action=action/connexion-action.php method=post>
+    <form action=".../src/Controller/ConnexionController.php" method=post>
         <label for=Courriel>Adresse Mail :</label>
         <input type=email name=Courriel class=text_zone required>
         <label for=Password>Mot de passe :</label>
@@ -8,3 +8,24 @@
         <input id=SubmitConnexion type=submit value=Envoyer>
     </form>
 </div>
+
+<?php
+/*public function login()
+{
+    // Enregistrer une variable de session
+    $this->request->getSession()->write('Utilisateur.nom', 'Jean Dupont');
+    
+    // Lire la variable de session
+    $nom = $this->request->getSession()->read('Utilisateur.nom');
+    echo $nom; // "Jean Dupont"
+
+    // Vérifier si la variable de session existe
+    if ($this->request->getSession()->check('Utilisateur.nom')) {
+        // La variable est définie
+    }
+
+    // Supprimer la variable de session
+    $this->request->getSession()->delete('Utilisateur.nom');
+}
+*/
+?>
